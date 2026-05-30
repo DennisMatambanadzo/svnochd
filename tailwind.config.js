f7d536b0a4b3ade86e70baa3c744450507628cfa
matambanadzotanakad <49873792+matazdt@users.noreply.github.com>
2026-05-30 11:17:41 +0200

    changed the footer to a grid in the mobile view, added a tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: ['max-h-0', 'max-h-48'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
