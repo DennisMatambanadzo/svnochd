e22d14ffc33b845b35f80b5ee6e265256a27a8ff
matambanadzotanakad <49873792+matazdt@users.noreply.github.com>
2026-06-01 19:46:16 +0200

    opening and closing cart menu

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const menuBtn = document.getElementById('menu-btn')
   const closeBtn = document.getElementById('close-btn')
   const mobileMenu = document.getElementById('mobile-menu')
+  const cartIconMobile = document.getElementById('cart-icon-mobile')
+  const cartMenuMobile = document.getElementById('cart-menu-mobile')
 
   menuBtn.addEventListener('click', () => {
     mobileMenu.classList.toggle('max-h-48')
@@ -15,5 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
     mobileMenu.classList.toggle('max-h-0')
     menuBtn.classList.toggle('hidden')
     closeBtn.classList.toggle('hidden')
+  })
+
+  cartIconMobile.addEventListener('click', () => {
+    cartMenuMobile.classList.toggle('max-h-96')
+    cartMenuMobile.classList.toggle('max-h-0')
+    // cartMenuMobile.classList.toggle('hidden') 
+    console.log(cartMenuMobile);
   })
 })
